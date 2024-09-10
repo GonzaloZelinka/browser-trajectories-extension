147ba9b1c3ff7c6d4d395398c392634a5c8c75f3
Gonzalo Zelinka <gonzalo.zelinka@scale.com>
2024-09-10 12:22:13 -0300

    remove old files

--- src/helpers.d.ts
+++
@@ -1,9 +0,0 @@
-import { SerializedAXNodeWithId } from "./src/types";
-export declare function getXPathForElement(element: Element): string;
-export declare function formatNode(node: SerializedAXNodeWithId, depth?: number): string;
-export declare function addIdHelper(node: SerializedAXNode, page: Page, idCounter: {
-    currentId: number;
-}): Promise<SerializedAXNodeWithId>;
-export declare function addId(node: SerializedAXNode, page: any, startingId?: number): Promise<SerializedAXNodeWithId>;
-export declare function showHighlight(element: Element): void;
-export declare function removeHighlight(): void;
